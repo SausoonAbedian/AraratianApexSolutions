d0f6e38ab1d9e955d204a55ff609448be6c1dedf
Sausoon Abedian <sausoon.abedian@araratianapexsolutions.com>
2024-08-15 00:23:26 -0700

    Decided to make things in one project for now as the application is small I didn't wanted to make things complicated for now

---
+++ AraratianApexSolutions.LeetCodeTracker/Program.cs
@@ -0,0 +1,34 @@
+namespace AraratianApexSolutions.LeetCodeTracker;
+
+using AraratianApexSolutions.LeetCodeTracker.Components;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+        WebApplication app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
