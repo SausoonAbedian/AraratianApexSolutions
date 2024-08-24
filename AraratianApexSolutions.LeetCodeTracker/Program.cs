cfc1d40aa4c840857c58e011e0ecec71b5fecb19
Sausoon Abedian <sausoon.abedian@araratianapexsolutions.com>
2024-08-23 23:32:15 -0700

    Added data flow from EF to pages

--- AraratianApexSolutions.LeetCodeTracker/Program.cs
+++ AraratianApexSolutions.LeetCodeTracker/Program.cs
@@ -1,18 +1,27 @@
 namespace AraratianApexSolutions.LeetCodeTracker;
 
+using System.IO;
+
 using AraratianApexSolutions.LeetCodeTracker.Components;
+using AraratianApexSolutions.LeetCodeTracker.Models;
+using AraratianApexSolutions.LeetCodeTracker.Services;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 public class Program
 {
+    public static readonly string dbConnectionString = $@"Server=(localdb)\mssqllocaldb;AttachDbFilename={Path.Combine(Directory.GetCurrentDirectory(), "Database", "LeetCodeTrackerDb.mdf")};Database=LeetCodeTrackerDb;Trusted_Connection=True;";
+
     public static void Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+        builder.Services.AddDbContext<LeetCodeTrackerDbContext>(options => options.UseSqlServer(dbConnectionString));
+        builder.Services.AddScoped(typeof(DataAccessService));
 
         WebApplication app = builder.Build();
 
---
+++ AraratianApexSolutions.LeetCodeTracker/Services/DataAccessService.cs
@@ -0,0 +1,15 @@
+﻿namespace AraratianApexSolutions.LeetCodeTracker.Services;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AraratianApexSolutions.LeetCodeTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class DataAccessService(LeetCodeTrackerDbContext context)
+{
+    public async Task<List<Question>> GetQuestionsAsync()
+    {
+        return await context.Questions.OrderBy(a => a.Number).ToListAsync();
+    }
+}
